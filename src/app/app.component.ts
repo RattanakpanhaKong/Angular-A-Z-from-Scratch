fff70bc163fa10431c7ef7621481d14752317a81
RattanakpanhaKONG <krattanakpanha@gmail.com>
2024-01-24 13:45:21 +0700

    Data sharing between child and parent using input/output/viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ɵclearResolutionOfComponentResourcesQueue } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'AngIntro';
+  parentMessage: string = 'Message changed';
+  message?: string;
+  fromChildViaEventEmit?: string;
+
+  @ViewChild(PostComponent) childComp: any;
+
+  constructor(){
+    console.log(this.childComp)
+  }
+
+
+  ngAfterViewInit(): void {
+    console.log(this.childComp)
+    this.message = this.childComp.childMessage
+  }
+  receivedMessage($event: any){
+    this.fromChildViaEventEmit = $event;
+  }
 }
